804d8f38b8251b993fdc9c38be2935e3a6f26dc0
Raul Conesa Sorolla <rconesasorolla@gmail.com>
2015-04-23 11:05:51 +0200

    last commit first day

--- src/Clases/Carton.java
+++ src/Clases/Carton.java
@@ -2,8 +2,16 @@ package Clases;
 
 import Interfaces.Marked;
 
+import java.sql.Array;
+import java.util.ArrayList;
+
 public class Carton implements Marked {
 
+    ArrayList<ArrayList<Integer>> carton = new ArrayList<ArrayList<Integer>>(3);
+
+    public Carton(){
+
+    }
 
     @Override
     public boolean isMarked() {
